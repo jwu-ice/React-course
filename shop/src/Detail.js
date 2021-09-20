12e153c51adb9a49afc2a514c3e709d91ae04b86
sju02048 <sju02048@naver.com>
2021-09-20 16:33:08 +0900

    reactwirj

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,37 +1,47 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import styled from "styled-components";
 import "./Detail.scss";
+import { storeContext } from "./App.js";
+import { Nav } from "react-bootstrap";
+import { CSSTransition } from "react-transition-group";
 
 let Box = styled.div`
   padding-top: 30px;
 `;
 
-let Naming = styled.h4`
-  font-size: 25px;
-  color: ${(props) => props.sacksang};
-`;
-
 function Detail(props) {
+  let [input, inputChange] = useState();
+
   let { id } = useParams();
   let history = useHistory();
+  let store = useContext(storeContext);
 
+  let [tab, setTab] = useState(0);
+  let [ani, setAni] = useState(false);
+
+  //
   let findProduct = props.shoes.find((a) => a.id == id);
+
+  function Info() {
+    return <p>재고 : {store[0]}</p>;
+  }
+
   return (
     <div className="container">
-      <Box>
-        여긴 박스자린데요
-        <Naming className="red">Detail Page</Naming>
-        <Naming sacksang="purple">Detail Page</Naming>
-      </Box>
-      <div className="my-alert2">
-        <p>재고가 얼마 남지 않았습니다.</p>
-      </div>
+      {input}
+      <input onChange={(e) => inputChange(e.target.value)} />
+
       <div className="row">
         <div className="col-md-6">
           <img
-            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            className="img_detail_shoes"
+            src={
+              "https://codingapple1.github.io/shop/shoes" +
+              (findProduct.id + 1) +
+              ".jpg"
+            }
             width="100%"
           />
         </div>
@@ -39,7 +49,16 @@ function Detail(props) {
           <h4 className="pt-5">{findProduct.title}</h4>
           <p>{findProduct.content}</p>
           <p>{findProduct.price} 원</p>
-          <button className="btn btn-danger">주문하기</button>
+          <Info />
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              props.setStore([9, 11, 12]);
+            }}
+          >
+            주문하기
+          </button>
+          &nbsp;
           <button
             className="btn btn-danger"
             onClick={() => {
@@ -50,8 +69,56 @@ function Detail(props) {
           </button>
         </div>
       </div>
+
+      <Nav className="mt-5" variant="tabs" defaultActiveKey="link-0">
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-0"
+            onClick={() => {
+              setAni(false);
+              setTab(0);
+            }}
+          >
+            as 1
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-1"
+            onClick={() => {
+              setAni(false);
+              setTab(1);
+            }}
+          >
+            as 2
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-2"
+            onClick={() => {
+              setAni(false);
+              setTab(2);
+            }}
+          >
+            as 2
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <CSSTransition in={ani} classNames="wow" timeout={500}>
+        <TabContent tab={tab} setAni={setAni} />
+      </CSSTransition>
     </div>
   );
 }
+
+const TabContent = (props) => {
+  useEffect(() => {
+    props.setAni(true);
+  });
+  if (props.tab === 0) return <div>0번째 내용</div>;
+  else if (props.tab === 1) return <div>1번째 내용</div>;
+  else if (props.tab === 2) return <div>2번째 내용</div>;
+};
 
 export default Detail;
